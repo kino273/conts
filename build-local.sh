7bad819c8e72e528611ba26ff6583a993d7f3011
Satoshi Kinoshita <kino273@gmail.com>
2016-06-22 13:48:37 +0900

    update

--- build-local.sh
+++ build-local.sh
@@ -4,6 +4,7 @@ ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 
 apt-get update
 apt-get install -y apt-utils
-apt-get install -y apt-file openssh-server sudo screen vim emacs less cron git \
+apt-get install -y --fix-missing \
+        apt-file openssh-server sudo screen vim emacs less cron git \
         curl wget make gcc ncurses-dev \
         python-pip python-dev python3 python3-pip
